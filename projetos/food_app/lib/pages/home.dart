7e3914a2f1d00caadcb17703bf3bb2ef4d503465
Moraes-Bruno <moraesbruno888@gmail.com>
2023-08-25 16:21:22 -0300

    header finalizado

--- projetos/food_app/lib/pages/home.dart
+++ projetos/food_app/lib/pages/home.dart
@@ -7,47 +7,68 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: appBar(),
-      body:  Column(
+      backgroundColor: Colors.white,
+      body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top: 40,left: 20,right: 20),
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(12, 0, 0, 0),
-                  blurRadius: 40,
-                  spreadRadius: 0.0
-                )
-              ]
-            ),
-            child:  TextField(
-              decoration: InputDecoration(
+          searchBar()
+        ],
+      ),
+    );
+  }
+
+  Container searchBar() {
+    return Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: const BoxDecoration(boxShadow: [
+            BoxShadow(
+                color: Color.fromARGB(12, 0, 0, 0),
+                blurRadius: 40,
+                spreadRadius: 0.0)
+          ]),
+          child: TextField(
+            decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
                 contentPadding: const EdgeInsets.all(15),
+                hintText: 'Search Pão de Queijo',
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 14
+                ),
                 prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset('/assets/icons/Search.svg')
+                    padding: const EdgeInsets.all(12),
+                    child: SvgPicture.asset('assets/icons/Search.svg')),
+                suffixIcon: Container( 
+                  width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SvgPicture.asset('assets/icons/Filter.svg')),
+                    ],
                   ),
+                )),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(15),
                   borderSide: BorderSide.none,
-                )
-              ),
-            ),
-          )
-        ],
-
-      ),
-    );
+                )),
+          ),
+        );
   }
 
   AppBar appBar() {
     return AppBar(
       title: const Text(
-        'Food App',
+        'Grab a Byte',
         style: TextStyle(
           color: Colors.black,
           fontSize: 18,
@@ -58,39 +79,36 @@ class HomePage extends StatelessWidget {
       elevation: 0.0,
       centerTitle: true,
       leading: GestureDetector(
-          onTap: (){
-
-          },
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10)
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow-Left2.svg',
+            width: 20,
+            height: 20,
+          ),
         ),
-        child: SvgPicture.asset('assets/icons/Arrow-Left2.svg',
-        width: 20,
-        height: 20,
-        ),
-      ),),
+      ),
       actions: [
         GestureDetector(
-          onTap: (){
-
-          },
-        child: Container(
-        margin: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        width: 37,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10)
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              width: 5,
+              height: 5,
+            ),
+          ),
         ),
-        child: SvgPicture.asset('assets/icons/dots.svg',
-        width: 5,
-        height: 5,
-        ),
-      ),),],
+      ],
     );
   }
 }
