f3194c727ea7ee1c0dd2bce0c3f788a3a4186e19
bruno <bruno@pop-os.localdomain>
2023-10-25 09:23:19 -0300

    desenvolvido contador

---
+++ estudo/contador/lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //chama o conteudo da pagina
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int contador = 0;
+
+  void incrementar(){
+    //Atualiza o estado da pagina
+    setState(() {
+      contador ++;
+    });
+  }
+
+  void decrementar(){
+    setState(() {
+      contador --;
+    });
+  }
+
+  //checa se o contador esta vazio (0)
+  bool get estaVazio => contador == 0;
+
+  //checa se o contador esta cheio(10)
+  bool get estaCheio => contador == 10;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return (Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("../assets/images/clay-banks-u27Rrbs9Dwc-unsplash.jpg"),
+          fit: BoxFit.cover)),
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                estaCheio ? "Lotado" : "Pode entrar",
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  decoration: TextDecoration.none
+                ),
+              ),
+               Padding(
+                padding: const EdgeInsets.all(40),
+                child: Text(
+                  "$contador",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: estaCheio ? Colors.black : Colors.white,
+                    fontWeight: FontWeight.w700,
+                    decoration: TextDecoration.none
+                  ),
+                ),
+              ),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: estaVazio ? null : decrementar,
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100,100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    child: const Text(
+                      "Saiu",
+                      style:TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      )
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  TextButton(
+                    onPressed: estaCheio ? null : incrementar,
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100,100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    child: const Text(
+                      "Entrou",
+                      style:TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      )
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+    ));
+  }
+}
+
